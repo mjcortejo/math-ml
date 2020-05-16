79b7afa4d5e28eec41032e6f5bf77f2300c2f3d3
Mark C <mark_jethro27@yahoo.com>
2020-05-17 00:53:41 +0800

    Create bisection.py

---
+++ bisection.py
@@ -0,0 +1,12 @@
+import math
+a = 1.0
+b = 2.0
+iterations = 15
+for iter in range(iterations):
+    if (b - a) == 1e-4: break
+    p = (a + b) / 2
+    eq = lambda x : math.pow(x, 3) + 4 * math.pow(x, 2) - 10
+    if eq(p) == 0.0: break
+    if eq(p) > 0: b = p
+    else: a = p
+    print("iter = {}\ta = {:.6f}\tb = {:.6f}\tc = {:.6f}\tfn(c) = {:.6f}\ttol = {:.6f}".format(iter+1, a, b, p, eq(p), (b - a)))
